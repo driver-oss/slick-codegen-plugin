f9c3158fa1bbb67e661b712c9269a7e8cfee60d3
Petro Verkhogliad <vpetro@gmail.com>
2016-09-22 15:59:16 -0400

    Output generated code into one file per schema

--- src/main/scala/CodegenPlugin.scala
+++ src/main/scala/CodegenPlugin.scala
@@ -7,26 +7,22 @@ object CodegenPlugin extends AutoPlugin {
   type TableColumn = (String, String)
 
   object autoImport {
-    lazy val codegen = TaskKey[Seq[File]]("gen-tables", "generate slick database schema")
+    lazy val codegen = TaskKey[Unit]("gen-tables", "generate slick database schema")
 
     lazy val codegenURI = SettingKey[String]("codegen-uri", "uri for the database configuration")
     lazy val codegenPackage = SettingKey[String]("codegen-package", "package in which to place generated code")
-    lazy val codegenTablesFile = SettingKey[String]("codegen-tables-file", "path for slick table models")
-    lazy val codegenRowsFile = SettingKey[String]("codegen-rows-file", "path for row case classes")
+    lazy val codegenOutputPath = SettingKey[String]("codegen-output-path", "directory to with the generated code will be written")
     lazy val codegenSchemaWhitelist = SettingKey[List[String]]("codegen-schema-whitelist", "schemas and tables to process")
     lazy val codegenForeignKeys = SettingKey[Map[TableColumn, TableColumn]]("codegen-foreign-keys", "foreign key references to data models add manually")
 
     lazy val slickCodeGenTask = Def.task {
-      NamespacedCodegen.run(
+      Generator.run(
         new java.net.URI(codegenURI.value),
         codegenPackage.value,
-        codegenTablesFile.value,
-        codegenRowsFile.value,
         codegenSchemaWhitelist.value,
+        codegenOutputPath.value,
         codegenForeignKeys.value
       )
-
-      Seq(file(codegenTablesFile.value), file(codegenRowsFile.value))
     }
   }
 }
