792f2401e29c728ce6730f54da15382bb567bd04
Petro Verkhogliad <vpetro@gmail.com>
2016-11-17 02:31:10 -0500

    Auto inject task and default settings

--- src/main/scala/CodegenPlugin.scala
+++ src/main/scala/CodegenPlugin.scala
@@ -7,28 +7,34 @@ object CodegenPlugin extends AutoPlugin {
   type TableColumn = (String, String)
 
   object autoImport {
-    lazy val codegen = TaskKey[Unit]("gen-tables", "generate slick database schema")
-
     lazy val codegenURI = SettingKey[String]("codegen-uri", "uri for the database configuration")
     lazy val codegenPackage = SettingKey[String]("codegen-package", "package in which to place generated code")
     lazy val codegenOutputPath = SettingKey[String]("codegen-output-path", "directory to with the generated code will be written")
     lazy val codegenSchemaWhitelist = SettingKey[List[String]]("codegen-schema-whitelist", "schemas and tables to process")
     lazy val codegenForeignKeys = SettingKey[Map[TableColumn, TableColumn]]("codegen-foreign-keys", "foreign key references to data models add manually")
 
-    lazy val slickCodeGenTask = Def.task {
-      Generator.run(
-        new java.net.URI(codegenURI.value),
-        codegenPackage.value,
-        Some(codegenSchemaWhitelist.value).filter(_.nonEmpty),
-        codegenOutputPath.value,
-        codegenForeignKeys.value
-      )
-    }
+    lazy val slickCodeGenTask = TaskKey[Unit]("gen-tables", "generate the table definitions")
 
-    lazy val baseCodegenSettings: Seq[Def.Setting[_]] = Seq(
-      codegen := slickCodeGenTask.value,
-      codegenSchemaWhitelist := List.empty,
-      codegenForeignKeys := Map.empty
-    )
   }
+
+  import autoImport._
+
+  override lazy val projectSettings = Seq(
+    codegenSchemaWhitelist := List.empty,
+    codegenForeignKeys := Map.empty,
+    slickCodeGenTask := Def.taskDyn {
+      Def.task {
+        Generator.run(
+          new java.net.URI(codegenURI.value),
+          codegenPackage.value,
+          Some(codegenSchemaWhitelist.value).filter(_.nonEmpty),
+          codegenOutputPath.value,
+          codegenForeignKeys.value
+        )
+      }
+    }.value
+
+  )
+
+
 }
