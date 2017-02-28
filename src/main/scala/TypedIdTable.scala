324bed11590c480d87a34ff5452e038c2a265b07
Stewart Stewart <stewinsalot@gmail.com>
2017-02-28 10:33:03 -0800

    scalafmt

--- src/main/scala/TypedIdTable.scala
+++ src/main/scala/TypedIdTable.scala
@@ -17,17 +17,19 @@ class TypedIdSourceCodeGenerator(
   )
 
   val keyReferences: Map[m.Column, m.Column] = {
-    val pks = databaseModel.tables.flatMap(_.columns)
+    val pks = databaseModel.tables
+      .flatMap(_.columns)
       .filter(_.options.contains(slick.ast.ColumnOption.PrimaryKey))
       .map(c => (c -> c))
 
-    val fks: Seq[(m.Column, m.Column)] = databaseModel.tables.flatMap(_.foreignKeys)
+    val fks: Seq[(m.Column, m.Column)] = databaseModel.tables
+      .flatMap(_.foreignKeys)
       .filter(_.referencedColumns.length == 1)
       .filter(_.referencedColumns.forall(
         _.options.contains(slick.ast.ColumnOption.PrimaryKey)))
       .flatMap(fk =>
-      fk.referencingColumns.flatMap(from =>
-        fk.referencedColumns.headOption.map(to => (from -> to))))
+        fk.referencingColumns.flatMap(from =>
+          fk.referencedColumns.headOption.map(to => (from -> to))))
 
     (pks ++ fks).toMap
   }
@@ -43,7 +45,12 @@ class TypedIdSourceCodeGenerator(
   }
 
   class TypedIdTable(model: m.Table) extends Table(model) { table =>
-    override def definitions = Seq[Def]( EntityType, PlainSqlMapper, TableClass, TableValue, PrimaryKeyMapper )
+    override def definitions =
+      Seq[Def](EntityType,
+               PlainSqlMapper,
+               TableClass,
+               TableValue,
+               PrimaryKeyMapper)
 
     class TypedIdColumn(override val model: m.Column) extends Column(model) {
       override def rawType: String = {
@@ -53,25 +60,28 @@ class TypedIdSourceCodeGenerator(
 
     type PrimaryKeyMapper = PrimaryKeyMapperDef
 
-    def PrimaryKeyMapper = new PrimaryKeyMapper { }
+    def PrimaryKeyMapper = new PrimaryKeyMapper {}
 
     class PrimaryKeyMapperDef extends TermDef {
       def primaryKeyColumn: Option[Column] = {
         table.model.columns
           .flatMap(c => keyReferences.get(c).filter(_ == c))
-          .headOption.map(c => table.columnsByName(c.name))
+          .headOption
+          .map(c => table.columnsByName(c.name))
       }
-        
+
       override def enabled = primaryKeyColumn.isDefined
 
-      override def doc = s"Implicit for mapping primary key of ${tableName(table.model.name.table)} to a base column"
+      override def doc =
+        s"Implicit for mapping primary key of ${tableName(table.model.name.table)} to a base column"
 
       override def rawName = tableName(table.model.name.table) + "KeyMapper"
 
       override def code = primaryKeyColumn.fold("") { column =>
         val tpe = s"BaseColumnType[${column.rawType}]"
         s"""|implicit def $name: $tpe = 
-            |${modelTypeToColumnMaper(column.model.tpe)}[${pKeyTypeTag(column.model)}]
+            |${modelTypeToColumnMaper(column.model.tpe)}[${pKeyTypeTag(
+             column.model)}]
             |""".stripMargin.lines.mkString("").trim
       }
     }
