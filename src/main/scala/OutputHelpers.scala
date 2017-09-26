a20d55523e6cb16611454253c34ef4cce6314779
Stewart Stewart <stewinsalot@gmail.com>
2017-09-25 19:22:19 -0700

    Don't instatiate slick data model automatically

--- src/main/scala/OutputHelpers.scala
+++ src/main/scala/OutputHelpers.scala
@@ -6,21 +6,12 @@ trait TableOutputHelpers extends TableFileGenerator with OutputHelpers { self: S
   def schemaName: String
   def imports: String
 
-  def packageTableCode(headerComment: String,
-                       pkg: String,
-                       schemaName: String,
-                       imports: String,
-                       profile: String): String =
+  def packageTableCode(headerComment: String, pkg: String, schemaName: String, imports: String): String =
     s"""|${headerComment.trim().lines.map("// " + _).mkString("\n")}
         |package $pkg
         |package $schemaName
         |
         |$imports
-        |
-        |/** Stand-alone Slick data model for immediate use */
-        |object tables extends {
-        |  val profile = $profile
-        |} with Tables
         |
         |/** Slick data model trait for extension, choice of backend or usage in the cake pattern. (Make sure to initialize this late.) */
         |trait Tables${parentType.fold("")(" extends " + _)} {
@@ -29,8 +20,8 @@ trait TableOutputHelpers extends TableFileGenerator with OutputHelpers { self: S
         |}
         |""".stripMargin.trim()
 
-  def writeTablesToFile(profile: String, folder: String, pkg: String, fileName: String): Unit = {
-    writeStringToFile(content = packageTableCode(headerComment, pkg, schemaName, imports, profile),
+  def writeTablesToFile(folder: String, pkg: String, fileName: String): Unit = {
+    writeStringToFile(content = packageTableCode(headerComment, pkg, schemaName, imports),
                       folder = folder,
                       pkg = s"$pkg.$schemaName",
                       fileName = fileName)
